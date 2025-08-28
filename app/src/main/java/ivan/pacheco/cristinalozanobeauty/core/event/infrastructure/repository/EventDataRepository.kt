22525d8779b384802dcbac46c604e5bfbb2a8214
Iván Pacheco <ipacheco@offshoretech.net>
2025-08-28 19:25:11 +0200

    Fix optional value endDateTime for get events

--- app/src/main/java/ivan/pacheco/cristinalozanobeauty/core/event/infrastructure/repository/EventDataRepository.kt
+++ app/src/main/java/ivan/pacheco/cristinalozanobeauty/core/event/infrastructure/repository/EventDataRepository.kt
@@ -47,7 +47,7 @@ class EventDataRepository @Inject constructor(
 
             // Filter events only with end date time
             response.items
-                .filter { it.end.dateTime != null }
+                .filter { it.end?.dateTime != null }
                 .map { item -> parseEvent(item) }
         }
     }
